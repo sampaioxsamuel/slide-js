33c54d5993d976f22220c27a3740474f7eb5da51
Samuel <sampaioxsamuel@gmail.com>
2020-09-18 17:55:06 -0300

    funcionando tanto com mouse quanto com touch

--- js/slide.js
+++ js/slide.js
@@ -20,24 +20,35 @@ export default class Slide {
   }
 
   onStart(event) {
-    event.preventDefault();
-    this.dist.startX = event.clientX;
-    this.wrapper.addEventListener("mousemove", this.onMove);
+    let moveType;
+    if (event.type === 'mousedown') {
+      event.preventDefault();
+      this.dist.startX = event.clientX;
+      moveType = 'mousemove';
+    } else {
+      this.dist.startX = event.changedTouches[0].clientX;
+      moveType = 'touchmove';
+    }
+    this.wrapper.addEventListener(moveType, this.onMove);
   }
 
   onMove(event) {
-    const finalPosition = this.updatePosition(event.clientX);
+    const pointerPostion = (event.type === 'mousemove') ? event.clientX : event.changedTouches[0].clientX;
+    const finalPosition = this.updatePosition(pointerPostion);
     this.moveSlide(finalPosition)
   }
 
   onEnd(event) {
-    this.wrapper.removeEventListener("mousemove", this.onMove);
+    const moveType = (event.type === 'mouseup') ? 'mousemove' : 'touchmove'
+    this.wrapper.removeEventListener(moveType, this.onMove);
     this.dist.finalPostion = this.dist.movePosition;
   }
 
   addSlideEvent() {
     this.wrapper.addEventListener("mousedown", this.onStart);
     this.wrapper.addEventListener("mouseup", this.onEnd);
+    this.wrapper.addEventListener("touchstart", this.onStart);
+    this.wrapper.addEventListener("touchend", this.onEnd);
   }
 
   bindEvents() {
