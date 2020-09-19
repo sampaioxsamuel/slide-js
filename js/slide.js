2deef05c4778951e5e2de39d45675396cae300e7
Samuel <sampaioxsamuel@gmail.com>
2020-09-19 18:07:11 -0300

    navegação prev e next por botões

--- js/slide.js
+++ js/slide.js
@@ -1,4 +1,4 @@
-import Debounce from './debounce.js'
+import debounce from './debounce.js'
 export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
@@ -138,7 +138,10 @@ export default class Slide {
     this.onStart = this.onStart.bind(this);
     this.onMove = this.onMove.bind(this);
     this.onEnd = this.onEnd.bind(this);
+
     this.onResize = debounce(this.onResize.bind(this), 200);
+    this.activePrevSlide = this.activePrevSlide.bind(this);
+    this.activeNextSlide = this.activeNextSlide.bind(this);
   }
 
   init() {
@@ -148,6 +151,19 @@ export default class Slide {
     this.addSlideEvent();
     this.addResizeEvent();
 
+    this.changeSlide(0);
     return this;
+  }
+}
+
+export class SlideNav extends Slide {
+  addArrow(prev, next) {
+    this.prevElement = document.querySelector(prev);
+    this.nextElement = document.querySelector(next);
+    this.addArrowEvent();
+  }
+  addArrowEvent() {
+    this.prevElement.addEventListener('click', this.activePrevSlide);
+    this.nextElement.addEventListener('click', this.activeNextSlide);
   }
 }
